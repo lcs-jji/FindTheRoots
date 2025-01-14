f44fc058cee2e62bca998ca5d9e03c2030b27643
Joseph Ji <jji@lcs.on.ca>
2025-01-14 11:09:51 -0500

    Initial Commit

---
+++ FindTheRoots/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FindTheRoots
+//
+//  Created by Xinchen Ji on 2025-01-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FindTheRoots/FindTheRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRootsApp.swift
+//  FindTheRoots
+//
+//  Created by Xinchen Ji on 2025-01-14.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
